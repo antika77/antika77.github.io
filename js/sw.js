f18508bcbf13f6b8a450015d8a6c595093d66a2e
antika77 <glasses91@naver.com>
2020-07-27 18:59:15 +0900

    11

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,30 @@
+//sw.js 파일 안
+//이 곳에서 polyfill을 구할 수 있습니다 : https://github.com/dominiccooney/cache-polyfill/blob/master/index.js
+importScripts('/js/serviceworker-cache-polyfill.js');
+
+//'install' 이벤트를 리스닝하며, 사이트 자산(assets)을 캐싱
+self.addEventListener('install', function(e) {
+	e.waitUntil(
+		caches.open('antika77.github.io').then(function(cache) {
+			return cache.addAll([
+				'/',
+				'/index.html',
+				// 필요한 자산(assets)들을 여기에 넣으세요
+			]);
+		})
+	);
+});
+
+//'fetch' 요청을 리스닝하며, 캐시에서 발견될 경우 캐시로부터 파일을 가져옴
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
+
+/*
 self.addEventListener('install', function(event) {
 	console.log('[Service Worker] Installing Service Worker ...', event);
 	
@@ -28,3 +55,4 @@ self.addEventListener('fetch', function(event) {
 		})
 	);
 });
+*/
